8a257ccae5fa9b1678f6133308b70084ea54a343
milmil12 <96069113+milmil12@users.noreply.github.com>
2022-01-08 23:39:06 +0700

    Update myapp-2.py

--- myapp-2.py
+++ myapp-2.py
@@ -54,12 +54,6 @@ source = ColumnDataSource(data={
 })
 
 
-# In[21]:
-
-
-# Output the visualization directly in the notebook
-# output_notebook()
-
 # Create the figure: plot
 plot = figure(title='1970', x_axis_label='Fertility (children per woman)', y_axis_label='Life Expectancy (years)',
            plot_height=400, plot_width=700, tools=[HoverTool(tooltips='@country')])
@@ -71,14 +65,8 @@ plot.circle(x='x', y='y', source=source, fill_alpha=0.8,
 # Set the legend and axis attributes
 plot.legend.location = 'bottom_left'
 
-# show(plot)
-
-
-# In[23]:
-
-
 # Define the callback function: update_plot
-def callback(attr, old, new):
+def update_plot(attr, old, new):
     # set the `yr` name to `slider.value` and `source.data = new_data`
     yr = slider.value
     x = x_select.value
@@ -99,17 +87,9 @@ def callback(attr, old, new):
     # Add title to figure: plot.title.text
     plot.title.text = 'Gapminder data for %d' % yr
 
-
-# In[24]:
-
-
 # Make a slider object: slider
 slider = Slider(start=1970, end=2010, step=1, value=1970, title='Year')
-slider.on_change('value',callback)
-
-
-# In[ ]:
-
+slider.on_change('value',update_plot)
 
 # Make dropdown menu for x and y axis
 # Create a dropdown Select widget for the x data: x_select
@@ -119,7 +99,7 @@ x_select = Select(
     title='x-axis data'
 )
 # Attach the update_plot callback to the 'value' property of x_select
-x_select.on_change('value', callback)
+x_select.on_change('value', update_plot)
 
 # Create a dropdown Select widget for the y data: y_select
 y_select = Select(
@@ -128,52 +108,8 @@ y_select = Select(
     title='y-axis data'
 )
 # Attach the update_plot callback to the 'value' property of y_select
-y_select.on_change('value', callback)
+y_select.on_change('value', update_plot)
     
-
-
-# In[ ]:
-
-
 # Create layout and add to current document
 layout = row(widgetbox(slider, x_select, y_select), plot)
 curdoc().add_root(layout)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-# bokeh serve --show myapp.py
-
-
-# For more on all things interaction in Bokeh, [**Adding Interactions**](https://docs.bokeh.org/en/latest/docs/user_guide/interaction.html) in the Bokeh User Guide is a great place to start.
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
